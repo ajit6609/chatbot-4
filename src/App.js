239169d6bbe69f8a65bb3f78569227b1edc9fa3f
ajit6609 <58482666+ajit6609@users.noreply.github.com>
2022-08-02 12:59:45 +0530

    code updated

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Chatbot from "./component/Chatbot";
+
+function App() {
+  return (
+    <div>
+      <h1>The Chatbot App</h1>
+      <Chatbot />
+    </div>
+  );
+}
+
+export default App;
